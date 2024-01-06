7527a5d794c20910fff1ba4ea08e3882fbcedac5
HanNl09 <hanly.novanto@gmail.com>
2024-01-06 14:18:05 +0700

    second version

--- pingpong.py
+++ pingpong.py
@@ -9,15 +9,20 @@ window = pygame.display.set_mode(RES)
 pygame.display.set_caption("Ping Fong")
 
 fps = pygame.time.Clock()
-frame = 30
+frame = 60
+
+# Game Variables
+paddle_speed = 3
+ball_speed = 2
+#
 
 class GameSprite(pygame.sprite.Sprite):
-    def __init__(self,playerImage,speed,coordinates,dimension=(50,50)):
+    def __init__(self,playerImage,speed,coordinates,dimension=(32,32)):
         super().__init__()
         self.image = pygame.transform.scale(
             pygame.image.load(playerImage+".png"),dimension
         )
-        self.speed = speed
+        self.speed_x,self.speed_y = speed,speed
         self.rect = self.image.get_rect()
         self.rect.x = coordinates[0]
         self.rect.y = coordinates[1]
@@ -31,21 +36,55 @@ class Ball(GameSprite):
         # if self.speed < 0:
         #     speedx = sqrt(fabs(self.speed)**2+self.direction**2)
         #     speedy = sqrt()
-        self.rect.x += self.speed
-        self.rect.y += self.speed
+        self.rect.x += self.speed_x
+        self.rect.y += self.speed_y
+        self.update()
+class Paddle(GameSprite):
+    def move(self,side):
+        key_pressed = pygame.key.get_pressed()
+        up = 0
+        down = 0
+        if side == "left":
+            if key_pressed[pygame.K_w]:
+                up = 0 - self.speed_y
+            if key_pressed[pygame.K_s]:
+                down = self.speed_y
+        else:
+            if key_pressed[pygame.K_UP]:
+                up = 0 - self.speed_y
+            if key_pressed[pygame.K_DOWN]:
+                down = self.speed_y
+        self.rect.y += up + down
         self.update()
 
-ball = Ball("ball",3,(width/2-25,height/2-25))
+ball = Ball("ball",ball_speed,(width/2-25,height/2-25))
+left_paddle = Paddle("blue_paddle",paddle_speed,(30,200),(25,100))
+right_paddle = Paddle("red_paddle",paddle_speed,(645,200),(25,100))
 
 while True:
-    window.fill((100,230,255))
+    window.fill((0,0,0))
 
     ball.move()
+    left_paddle.move("left")
+    right_paddle.move("right")
+    
+    if pygame.sprite.collide_rect(left_paddle,ball):
+        ball.rect.x += (left_paddle.rect.x+left_paddle.rect.width) - ball.rect.x
+        ball.speed_x *= -1
+    if pygame.sprite.collide_rect(right_paddle,ball):
+        ball.rect.x -= (ball.rect.x+ball.rect.width)-right_paddle.rect.x
+        ball.speed_x *= -1
+    if ball.rect.y < 0:
+        ball.rect.y -= ball.rect.y
+        ball.speed_y *= -1
+    if ball.rect.y > (height-ball.rect.height):
+        ball.rect.y -= ball.rect.y - (height-ball.rect.height)
+        ball.speed_y *= -1
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             pygame.quit()
             quit()
-    
+
     pygame.display.update()
     fps.tick(frame)
