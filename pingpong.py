c5726125273e2e9ab61493496277bad713269b9c
HanNl09 <hanly.novanto@gmail.com>
2024-01-10 18:38:58 +0700

    second commit

--- pingpong.py
+++ pingpong.py
@@ -3,6 +3,7 @@ from math import fabs,sqrt
 from random import randint
 
 pygame.init()
+pygame.font.init()
 
 RES = width,height = 700,500
 window = pygame.display.set_mode(RES)
@@ -14,6 +15,12 @@ frame = 60
 # Game Variables
 paddle_speed = 3
 ball_speed = 2
+
+left_score = 0
+right_score = 0
+#
+# Fonts
+score = pygame.font.SysFont("calibri",40)
 #
 
 class GameSprite(pygame.sprite.Sprite):
@@ -33,12 +40,21 @@ class Ball(GameSprite):
     #     super().__init__()
     #     self.direction = direction
     def move(self):
+        global left_score,right_score
         # if self.speed < 0:
         #     speedx = sqrt(fabs(self.speed)**2+self.direction**2)
         #     speedy = sqrt()
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
         self.update()
+        if self.rect.x < 0:
+            left_score += 1
+            self.rect.x = width/2-self.rect.width/2
+            self.rect.y = height/2-self.rect.height/2
+        if (self.rect.x+self.rect.width) > width:
+            right_score += 1
+            self.rect.x = width/2-self.rect.width/2
+            self.rect.y = height/2-self.rect.height/2
 class Paddle(GameSprite):
     def move(self,side):
         key_pressed = pygame.key.get_pressed()
@@ -80,6 +96,8 @@ while True:
     if ball.rect.y > (height-ball.rect.height):
         ball.rect.y -= ball.rect.y - (height-ball.rect.height)
         ball.speed_y *= -1
+    
+    window.blit(score.render((str(left_score)+":"+str(right_score)),True,(255,255,255)),(width/2-30,20))
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
